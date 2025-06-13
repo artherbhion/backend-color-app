8d726fc0b156f7c7d5caa6dd44872138ad24fcb8
Arther Bhion C <147584933+artherbhion@users.noreply.github.com>
2025-06-13 10:44:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ origins = [
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # Allow all domains
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
